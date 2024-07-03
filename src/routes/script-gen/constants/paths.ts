84da2bb41cfc3235f25209fd5775df0b13515d17
RecursionExcursion <rloup15@gmail.com>
2024-07-03 12:02:27 -0400

    routing

--- src/routes/script-gen/constants/paths.ts
+++ src/routes/script-gen/constants/paths.ts
@@ -1,2 +1,8 @@
-export const ROUTE = "src/routes/script-gen";
+import path from "path";
+
+const absPath = path.resolve("./");
+
+console.log({ __dirname, __filename, absPath });
+
+export const ROUTE = absPath + "/src/routes/script-gen";
 export const PRE_SCRIPTS = ROUTE + "/pre-scripts";
